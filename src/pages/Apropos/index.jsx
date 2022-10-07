ff5cdbaad24098efaa32dd5fb89bd665afa170af
Moustapha FALL <moustapha.fall.external@worldline.com>
2022-10-07 12:08:32 +0000

    commencer la page A propos

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -1,8 +1,39 @@
-function Propos(){
-    return (
-        <div>
-            <h1>Voici ma page A Propos</h1>
-        </div>
-    )
+import styled from "styled-components";
+import ProposImageAccueil from "../../assets/apropos.png";
+
+
+const ProposWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+// <ImageTitle>Chez vous, partout et ailleurs</ImageTitle>
+const ProposContainer = styled.div`
+  margin: 30px;
+  padding: 60px 90px;
+  display: grid;
+  flex-direction: row;
+  max-width: 1200px;
+`;
+const ProposAccueil = styled.img`
+  border-radius: 25px;
+  width: 1150px;
+  height: 223px;
+  left: 100px;
+  top: 171px;
+  mix-blend-mode: darken;
+  color: #000000;
+  opacity: 1;
+  background: #e5e5e5;
+`;
+
+
+function Propos() {
+  return (
+    <ProposWrapper>
+      <ProposContainer>
+        <ProposAccueil src={ProposImageAccueil} />
+      </ProposContainer>
+    </ProposWrapper>
+  );
 }
 export default Propos
